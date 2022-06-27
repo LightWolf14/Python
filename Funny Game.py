6159f634be3dbfe4d54701c1e82062d762efbdcd
kukku1890 <rai.devanshi11@gmail.com>
2022-06-27 11:02:25 +0530

    Python is the best!

---
+++ Funny Game.py
@@ -0,0 +1,29 @@
+# This is a story creator game, solely created by Python 3.0 software.
+
+name = (input('Add a name for a character'))
+food = (input('Add a food for the character'))
+place = (input('Add a place for the character'))
+favourite_object = (input('Add a favourite object for the character'))
+
+print('''
+
+Hi my name is ,''' + name)
+
+print('''
+
+My favourite food is ''' + food)
+
+print('''
+
+I live in ''' + place)
+
+print('''
+
+My favourite object is ''' + favourite_object)
+
+End_story_statement = (input('Did you like the story??? I added a funny twist to it!'))
+
+# if End_story_statement == Y:
+# print ("Well, I am glad you liked it! Bye!!!")
+# else:
+# print ("Oh well, I tried my best... I am a machine after all! We could never be like you... Bye...")
--- Lists_Tuples_and_Sets.py
+++ Lists_Tuples_and_Sets.py
@@ -1,8 +1,1 @@
-Family = ['Papa', 'Mumma', 'Sanchu', 'Me']
 
-Family_str = ' ~ '.join(Family)
-
-
-print(Family_str)
-
-# Video for tomorrow: - ('')

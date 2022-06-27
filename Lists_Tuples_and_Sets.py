d10b6b10e8b6470e9fbe72ac57fff22a47c78bc6
kukku1890 <rai.devanshi11@gmail.com>
2022-06-27 11:23:29 +0530

    Python is the best!

--- Lists_Tuples_and_Sets.py
+++ Lists_Tuples_and_Sets.py
@@ -2,17 +2,17 @@
 
 # Q.1 - Reverse this given list =
 
-#list1 = [100, 200, 300, 400, 500]
+# list1 = [100, 200, 300, 400, 500]
 
-#list1.reverse()
+# list1.reverse()
 
-#print(list1)
+# print(list1)
 
 # Q.2 - Given a list of numbers. write a program to turn every item of a list into its square.
 
-numbers = [1, 2, 3, 4, 5, 6, 7]
-result = []
-for num in numbers:
-    result.append(num * num)
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+# result = []
+# for num in numbers:
+# result.append(num * num)
 
-print (result)
+# print (result)
